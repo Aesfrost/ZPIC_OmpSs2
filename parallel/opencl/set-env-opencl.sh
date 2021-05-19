337c9705797499cb28110654c95a02e25a2a8042
nlg550 <38725499+nlg550@users.noreply.github.com>
2021-05-19 17:03:40 -0300

    Removed opencl version
    The opencl implementation is too old... I will replace with the newest version

--- parallel/opencl/set-env-opencl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CPATH=/opt/rocm-3.8.0/opencl/include/
-export LD_LIBRARY_PATH=/opt/rocm-3.8.0/opencl/lib/x86_64:/opt/rocm-3.8.0/lib/
-export OPENCL_VENDOR_PATH=/home/nguidotti/tools/
-export PATH=/opt/rocm-3.8.0/bin/:$PATH
-export PATH=/home/nguidotti/tools/bin:$PATH
